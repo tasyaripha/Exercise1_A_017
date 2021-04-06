1f0a2950a0c4e909d8f01ff25c22182aeafff012
Rayka Agustasya <raykaagustasya16@gmail.com>
2021-04-07 03:56:14 +0700

    add popup_menu.xml

--- app/src/main/java/com/example/exercise1/data_kontak.java
+++ app/src/main/java/com/example/exercise1/data_kontak.java
@@ -2,13 +2,33 @@ package com.example.exercise1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.SearchView;
 
-public class data_kontak extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class data_kontak extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener, View.OnClickListener {
+    Bundle bundle = new Bundle();
+    Intent intent;
+    private SearchView tCari;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_kontak);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        return false;
     }
 }
--- app/src/main/java/com/example/exercise1/pendaftaran.java
+++ app/src/main/java/com/example/exercise1/pendaftaran.java
@@ -1,10 +1,15 @@
 package com.example.exercise1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class pendaftaran extends AppCompatActivity {
 
@@ -12,7 +17,14 @@ public class pendaftaran extends AppCompatActivity {
 
     EditText enama, eAlamat, edEmail, edPassword, edrePassword;
 
-    String Nama, Alamat, JenisKelamin;
+    String Nama, Alamat, JenisKelamin, Agama;
+
+    TextInputLayout dafnama, dafalamat, PDaftar1, PDafpass, Pdafrepass;
+    RadioButton Jk1, JK2, rbIs, rbKi, rbKr, rbHi, rbKg, rbBd, rbPk;
+    int Jk;
+    RadioGroup rgAgama;
+    RadioGroup rgAgama2;
+    RadioGroup rgAgama3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +33,24 @@ public class pendaftaran extends AppCompatActivity {
 
         btnCancel = findViewById(R.id.btnCancel);
         btnRegistrasi = findViewById(R.id.btnRegistrasi);
+
+
+    }
+
+    public void JenisKelamin(View view) {
+        int i = this.Jk;
+        if (i == R.id.Jk1) {
+            this.JenisKelamin = "Laki-Laki";
+        } else if (i == R.id.Jk2) {
+            this.JenisKelamin = "Perempuan";
+        }
+    }
+    public void validasi (View view) {
+        if (this.enama.getText().toString().isEmpty() || this.eAlamat.getText().toString().isEmpty()
+                || this.edEmail.getText().toString().isEmpty() || this.edPassword.getText().toString().isEmpty()
+                || this.edrePassword.getText().toString().isEmpty() || this.Agama.isEmpty() || this.JenisKelamin.isEmpty());
+        {
+            Toast.makeText(getApplicationContext(),"Data Harus DiIsi Semua", 0).show();
+        }
     }
 }
