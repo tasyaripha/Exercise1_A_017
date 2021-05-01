3e4b84ecd56b136beeae9d1a21159d4ffb4028d3
Rayka Agustasya <raykaagustasya16@gmail.com>
2021-05-02 01:56:53 +0700

    Exercise 2

--- app/src/main/java/com/example/exercise1/MainActivity.java
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -1,28 +1,34 @@
 package com.example.exercise1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class MainActivity extends AppCompatActivity {
-
-    //Deklarasi variabel untuk button
     Button btnLogin;
-
-    //Deklarasi variabel untuk EditText
     EditText edEmail, edPassword;
-
-    //Deklarasi variabel untuk menyimpan email dan password
     String nama, password;
+    TextInputLayout emailError, passError;
+    boolean isEmailValid, isPasswordValid;
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvRegister = findViewById(R.id.tvRegister);
 
         //Menghubungkan variabel btnLogin dengan componen button pada layout
         btnLogin = findViewById(R.id.button1);
@@ -37,44 +43,44 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                //Menyimpan input user di edittext email kedalam variabel nama
-                nama = edEmail.getText().toString();
-
-                //Menyimpan input user di edittext password kedalam variabel password
-                password = edPassword.getText().toString();
-
-                //Set Email yang benar
-                String email = "admin@mail.com";
-
-                //Set Password yang benar
-                String pass = "123456";
-
-                if (nama.isEmpty() || password.isEmpty()) {
-                    //membuat variabel toast dan membuat toast dengan menambahkan variabel nama dan password
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Email atau Password Salah", Toast.LENGTH_LONG);
-                    //menampilkan toast
-                    t.show();
-                } else {
-                    if (nama.equals(email) && password.equals(pass)) {
-                        Toast t = Toast.makeText(getApplicationContext(),
-                                "Login Sukses", Toast.LENGTH_LONG);
-                        t.show();
-                        Bundle b = new Bundle();
-
-                        b.putString("a", nama.trim());
-
-                        b.putString("b", password.trim());
-
-                        Intent i = new Intent(getApplicationContext(), data_kontak.class);
-
-                        i.putExtras(b);
-
-                        startActivity(i);
-                    }
-                }
+                MainActivity.this.SetValidation();
             }
         });
+        this.tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startActivity(new Intent(MainActivity.this.getApplicationContext(), pendaftaran.class));
+            }
+        });
+    }
+//
+//    @SuppressLint("WrongConstant")
+    private void SetValidation() {
+        if (this.edEmail.getText().toString().isEmpty()) {
+            this.emailError.setError("Email Tidak Boleh Kosong");
+            this.isEmailValid = false;
+        } else if (Patterns.EMAIL_ADDRESS.matcher(this.edEmail.getText().toString()).matches()) {
+            this.emailError.setError("Harap Masukkan Email Dengan Benar");
+            this.isEmailValid = false;
+        } else {
+            this.isEmailValid = true;
+            this.emailError.setErrorEnabled(false);
+        }
+        if (this.edPassword.getText().toString().isEmpty()) {
+            this.passError.setError("Password Tidak Boleh Kosong");
+            this.isPasswordValid = false;
+        } else if (this.edPassword.getText().length() < 6) {
+            this.passError.setError("Panjang Password Minimal 6 Karakter");
+            this.isPasswordValid = false;
+        } else {
+            this.isPasswordValid = true;
+            this.passError.setErrorEnabled(false);
+        }
+        if (this.isEmailValid || !this.isPasswordValid || this.edEmail.getText().toString().equals("admin@mail.com") || this.edPassword.getText().toString().equals("123456")) {
+            Toast.makeText(getApplicationContext(), "Email atau Password Salah", 0).show();
+            return;
+        }
+        Toast.makeText(getApplicationContext(), "Login Sukses", 0).show();
+        startActivity(new Intent(getApplicationContext(), data_kontak.class));
     }
 }
